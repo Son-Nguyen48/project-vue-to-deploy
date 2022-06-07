f1e7563d8e37fecf5ebc5e96dcb3a8fcac8fac70
Son-Nguyen48 <sonnt.B16CN303@stu.ptit.edu.vn>
2022-06-07 17:18:41 +0700

    nothing

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: "/new-vue-project-to-deploy/",
+  publicPath: "/new-vue-project-to-deploy/"
 });
